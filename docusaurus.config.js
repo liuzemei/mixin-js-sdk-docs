fb2b3577dff240a9bb9e152b33e7f07e463bee21
Neo <her_liu@qq.com>
2022-08-08 13:38:40 +0800

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,109 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+
+const config = {
+  title: 'Mixin JS SDK',
+  tagline: 'Mixin JS SDK Documents',
+  url: 'https://mixin-web.docs.mixinbots.com',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Mixin',
+  projectName: '@mixin.dev/web',
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  // i18n: {
+  //   defaultLocale: 'zh-CN',
+  //   locales: ['en', 'zh-CN'],
+  //   localeConfigs: {
+  //     'en': {
+  //       label: 'English',
+  //     },
+  //     'zh-CN': {
+  //       label: '简体中文',
+  //     }
+  //   }
+  // },
+  themeConfig:
+    ({
+      navbar: {
+        title: 'Mixin JS SDK',
+        logo: {
+          alt: 'Mixin JS SDK',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'server/test',
+            position: 'left',
+            label: 'Server',
+          },
+          {
+            type: 'doc',
+            docId: 'web/intro',
+            position: 'left',
+            label: 'Web',
+          },
+          // {
+          //   type: "localeDropdown",
+          //   position: 'right',
+          // },
+          {
+            href: 'https://github.com/liuzemei/mixin-js-sdk/tree/new-sdk',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: '快速开始',
+                to: '/docs/server/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Mixin Developers',
+                href: 'https://developers.mixin.one',
+              },
+              {
+                label: 'Mixin HomePage',
+                href: 'https://mixin.one',
+              },
+            ],
+          }
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Mixin, Inc.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+}
+
+module.exports = config
